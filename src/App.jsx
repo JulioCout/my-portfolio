d75c2f6e3325d3c7bcdf6ae9edfef434edd11fe8
Julio Coutinho <juliosilvacoutinho@gmail.com>
2022-08-21 22:04:01 -0300

    html and css modification

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,27 @@ function App() {
 
   return (
     <div className="App">
-      <div className="gradient1"></div>
-        <div className="gradient2"></div>
+      <div className="gradient1" ></div>
+      <div className="gradient2" ></div>
+      {/* <img className="obj-1" src="./src/assets/SuperToroid-Purple-Glossy.svg" /> */}
+      <img className="obj-2" src="./src/assets/Sphere-White-Matte.svg" />
+      <img className="obj-3" src="./src/assets/Pill-Blue-Glossy.svg" />
+      <img className="obj-4" src="./src/assets/RoundCube-Orange-Glossy.svg" />
+      <img className="obj-5" src="./src/assets/SuperToroid-Yellow-Glossy.svg" />
         
-        <div className="head-wrapper">
-            <div className="text-wrapper">
-                <h1>Julio <span>C. S.</span> Coutinho</h1>
-                <p>Desenvolvedor Web <span>Full Stack</span></p>
+        <div className="head-container">
+            <div className="text-container">
+                <img src="./src/assets/profile-pic.png" alt="" />
+                <h1>Olá! Eu sou o Julio.</h1>
+                <h2> <span>Full Stack</span> Web Developer</h2>
                 <button>LinkedIn</button>
             </div>
-            <img src="https://avatars.githubusercontent.com/u/90656852?v=4" alt="" />
+            <img src="./src/assets/Saly-13.png" className="character" alt="" />
+            
         </div>
 
 
-        <div className="section-wrapper">
+        <div className="section">
             <h2>Algumas de minhas habilidades</h2>
             
             <h3>Front-end</h3>
@@ -32,7 +39,7 @@ function App() {
         </div>
         
 
-        <div className="section-wrapper">
+        <div className="section">
             <h2>Meus Projetos</h2>
     
             <h3>Full Stack</h3>
@@ -46,7 +53,7 @@ function App() {
             </div>
         </div>
 
-        <div className="section-wrapper">
+        <div className="section">
             <h2>Sobre mim</h2>
             <p>
                 Sempre fui entusiasta de tecnologia e estou em transição de carreira para a área de programação web. Sou pós-graduado em engenharia de software e desenvolvo Frontend com JavaScript, HTML, CSS e React. Além disso, desenvolvo Backend com Node.js, Express e SQL. Tenho contato com desenvolvimento web há 11 anos como hobby e agora estou me profissionalizando na área.
