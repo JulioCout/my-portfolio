75dc3f662b80ecee9390062c2fa65717065166e6
Julio Coutinho <juliosilvacoutinho@gmail.com>
2023-12-06 11:53:59 -0300

    Update index.jsx

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -14,17 +14,9 @@ export function AboutSection({ globalLangPortuguese }) {
                     <h2 className="about-title">{globalLangPortuguese ? "Sobre mim" : "About me"}</h2>
                     <p className="about-text">
                         {
-                            globalLangPortuguese ? "Pós-Graduado em Engenharia de Software com experiência em projetos para Web com foco em Front End com JavaScript, HTML, CSS e React. Além disso, possuo conhecimentos em Scrum, TypeScript, Git, Sass, Styled Components, Figma, NodeJs, SQL entre outras tecnologias e bibliotecas."
+                            globalLangPortuguese ? "Técnico de Automação Industrial a 10 anos com experiência em lógica de programação, linguagem C++ e Ladder. Pós-graduado em Engenharia de Software com experiência no desenvolvimento backend com Python, Django, Node.js, SQL, noSQL, entre outras tecnologias e bibliotecas."
                             :
-                            "Postgraduate in Software Engineering with experience in Web projects focused on Front End with JavaScript, HTML, CSS and React. Also, I have knowledge in Scrum, TypeScript, Git, Sass, Styled-Components, Figma, NodeJs, SQL among other technologies and libraries."
-                        }
-                    </p>
-                    
-                    <p className="about-text">
-                        {
-                            globalLangPortuguese ? "Possuo conhecimento na área de desenvolvimento web desde 2011, quando era Web Designer e criava sites como freelancer. Atualmente estou me especializando em JavaScript para regressar a área de desenvolvimento web. Nos últimos seis anos atuei na área de supervisão de manutenção e automação."
-                            :
-                            "I have knowledge in the area of web development since 2011, when I was a Web Designer and created websites as a free lancer. I'm currently specializing in JavaScript to return to web development. In the last 6 years I worked in the area of maintenance supervision and automation. I studied English for 8 years, and can understand and communicate in this language."
+                            "Industrial Automation Technician for 10 years with experience in programming logic, C++ and Ladder languages. Postgraduate in Software Engineering with experience in backend development with Python, Django, Node.js, SQL, noSQL, among other technologies and libraries."
                         }
                     </p>
                 </div>
